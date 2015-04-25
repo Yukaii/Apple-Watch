9d8365f36609c546e7fbbb1093362a919f77b400
Yukai Huang <yukaihuang1993@hotmail.com>
2015-04-25 19:09:41 +0800

    make today's news

--- app/models/news.rb
+++ app/models/news.rb
@@ -3,6 +3,7 @@ class News < ActiveRecord::Base
 
   default_scope { order(created_at: :desc) }
   scope :shiyijei, -> { where(author: "施旖婕") }
+  scope :today, -> { where('DATE(published_at) = ?', Date.today) }
 
   def save_author
     if self.content && self.author.nil?
