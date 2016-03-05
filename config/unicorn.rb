27fff7ea997475a2b011acd3a11b28416a70298a
Yukai Huang <yukaihuang1993@hotmail.com>
2016-03-05 10:00:10 +0800

    now can use smaller timeout

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 # https://devcenter.heroku.com/articles/rails-unicorn
 
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout 600
+timeout 15
 preload_app true
 
 before_fork do |server, worker|
